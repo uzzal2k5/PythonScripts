936e130a8f4a100b6364a72580b805bf2912cc09
Uzzal <uzzal2k5@gmail.com>
2016-05-25 16:50:54 +0600

    System Info Gathering

--- pysysinfo.py
+++ pysysinfo.py
@@ -1,37 +1,39 @@
 #!/usr/local/bin/python3.4
-#A System Information Gathering Script
+# A System Information Gathering Script
 import subprocess
 
-#Command 1
-def uname_func():
 
+# Command 1
+def uname_func():
     uname = "uname"
     uname_arg = "-a"
-    print ("Gathering system information with %s command:\n" % uname)
+    print ('Gathering system information with %s command:', uname)
     subprocess.call([uname, uname_arg])
 
-#Command 2
-def disk_func():
 
+# Command 2
+def disk_func():
     diskspace = "df"
     diskspace_arg = "-h"
-    print "Gathering diskspace information %s command:\n" % diskspace
+    print('Gathering diskspace information %s command:',diskspace)
     subprocess.call([diskspace, diskspace_arg])
 
-#Command 3
+
+# Command 3
 def memory_func():
-    
     mem = "free"
     mem_arg = "-h"
-    print "Memory Information %s Command:\n "  %  mem
-    subprocess.call([mem,  mem_arg])
+    print('Memory Information {0:s} Command: ',mem)
+    subprocess.call([mem, mem_arg])
 
-#Main function that call other functions
+
+# Main function that call other functions
 def main():
     # type: () -> object
     # type: () -> object
     uname_func()
     disk_func()
     memory_func()
+
 
 main()
